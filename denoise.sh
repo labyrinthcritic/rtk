b41415916c447b76648295eedcecf14ccd5e25ac
labyrinthcritic <labyrinthcritic@gmail.com>
2024-01-19 13:26:53 -0500

    corrected arguments for oidnDenoise

--- denoise.sh
+++ denoise.sh
@@ -3,7 +3,7 @@
 image_path=$1
 
 magick convert "$image_path" -endian LSB PFM:image.pfm
-oidnDenoise -hdr image.pfm -o denoised.pfm
+oidnDenoise --srgb --quality high -ldr image.pfm -o denoised.pfm
 magick convert denoised.pfm "PNG:denoised.png"
 rm image.pfm
 rm denoised.pfm
