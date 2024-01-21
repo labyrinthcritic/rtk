af8b2f62155a0805b3caa7d0466aebd1b4ff9ecd
labyrinthcritic <labyrinthcritic@gmail.com>
2024-01-20 22:17:00 -0500

    added check for path

--- denoise.sh
+++ denoise.sh
@@ -4,6 +4,12 @@ set -e
 
 image_path=$1
 
+if [ -z $image_path ]
+then
+  echo 'usage: ./denoise.sh <path>'
+  exit 1
+fi
+
 magick convert "$image_path" -endian LSB PFM:image.pfm
 oidnDenoise --srgb --quality high -ldr image.pfm -o denoised.pfm
 magick convert denoised.pfm "PNG:denoised.png"
